8475d0019443f8a07cb64bde39fa4b05cbd47eb3
Christian Escobar <crs.escobar@gmail.com>
2020-07-04 17:25:07 -0700

    work in progress

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
+'use strict';
+
 const inquirer = require('inquirer');
+const { Retire } = require('./process/Retire');
 
 const choices = [
 	'I plan to retire',
@@ -20,13 +23,13 @@ const start = async function start() {
 	const userInput = await inquirer.prompt(questions);
 	const { situation } = userInput;
 	if (situation === choices[0]) {
-		console.log('Retire chosen');
+		new Retire().run();
 	} else if (situation === choices[1]) {
 		console.log('Separate');
 	} else if (situation === choices[2]) {
 		console.log('Honorably involuntarily separated');
 	} else {
-		throw new Error(`Unhandled choice encountered ${situation}`);
+		throw new Error(`Unhandled situation selection encountered ${situation}`);
 	}
 };
 
---
+++ src/process/Retire.js
@@ -0,0 +1,108 @@
+'use strict';
+
+const inquirer = require('inquirer');
+const moment = require('moment');
+
+const validateFirstOfMonthDate = function validateFirstOfMonthDate(value) {
+	const pass = value.match(/01\/\d{2}\/\d{4}/);
+	if (pass) {
+		return true;
+	}
+
+	return 'Retirement date must be the first day of the month';
+};
+
+const validateDate = function validateDate(value) {
+	const pass = value.match(/\d{2}\/\d{2}\/\d{4}/);
+	if (pass) {
+		return true;
+	}
+
+	return 'Invalid date format';
+};
+
+const calculateRetirementDate = function calculateRetirementDate(
+	enlistOrCommissionDate
+) {
+	try {
+		// Make sure this is a valid date
+		const momentEnlistOrCommDate = moment(enlistOrCommissionDate, 'DD/MM/YYYY');
+		momentEnlistOrCommDate.add(1, 'months').startOf('month');
+	} catch (error) {
+		throw new Error(
+			`Error encountered while attempting to calculate retirement date.\n${error}`
+		);
+	}
+};
+
+const retirementDateChoices = [
+	'Enter date (01/MM/YYYY)',
+	'Calculate 20 years in for me',
+];
+
+const planToRetire = {
+	type: 'list',
+	name: 'planToRetire',
+	message: 'When do you plan to retire?',
+	choices: retirementDateChoices,
+};
+
+const enterRetirementDate = {
+	type: 'input',
+	name: 'retirementDate',
+	message: 'Enter date (01/MM/YYYY)',
+	validate: validateFirstOfMonthDate,
+};
+
+const enterEnlistmentOrCommissionDate = {
+	type: 'input',
+	name: 'enlistOrCommissionDate',
+	message: 'Enter your enlistment or commissioning date (DD/MM/YYYY)',
+	validate: validateDate,
+};
+
+const promptUser = async function promptUser() {
+	const userInput = {
+		retirementDate: '',
+		conusOrOcunus: '',
+		daysOfLeave: 0,
+	};
+
+	const retirementDateChoice = await inquirer.prompt(planToRetire);
+
+	const { planToRetire: dateSource } = retirementDateChoice;
+	if (dateSource === retirementDateChoices[0]) {
+		const retirementDateInput = await inquirer.prompt(enterRetirementDate);
+		userInput.retirementDate = retirementDateInput.retirementDate;
+	} else if (dateSource === retirementDateChoices[1]) {
+		// Calculate 20 years
+		const enlistOrCommissionDateInput = await inquirer.prompt(
+			enterEnlistmentOrCommissionDate
+		);
+		console.log(`---> ${JSON.stringify(enlistOrCommissionDateInput, null, 2)}`);
+		const { enlistOrCommissionDate } = enlistOrCommissionDateInput;
+		calculateRetirementDate(enlistOrCommissionDate);
+	} else {
+		throw new Error(`Unhandled planToRetire encountered ${planToRetire}`);
+	}
+
+	console.log(userInput);
+};
+
+class Retire {
+	constructor() {
+		this.situation = 'I plan to retire';
+		this.plannedRetirementDate = '';
+		this.CONUS = false;
+		this.OCUNUS = false;
+		this.daysOfLeave = 0;
+	}
+
+	async run() {
+		const userInput = await promptUser();
+	}
+}
+
+module.exports = {
+	Retire,
+};
