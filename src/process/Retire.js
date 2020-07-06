2a2475bdf03ac37bbecb1f216bbc66761326d251
Christian Escobar <crs.escobar@gmail.com>
2020-07-05 18:31:03 -0700

    additional prompts and days of leave in progress

--- src/process/Retire.js
+++ src/process/Retire.js
@@ -21,6 +21,15 @@ const validateDate = function validateDate(value) {
 	return 'Invalid date format';
 };
 
+const validateInputIsNumbersOnly = function validateInputIsNumbersOnly(value) {
+	const pass = value.match(/\d+/);
+	if (pass) {
+		return true;
+	}
+
+	return 'Only numbers are allowed';
+};
+
 const calculateRetirementDate = function calculateRetirementDate(
 	enlistOrCommissionDate
 ) {
@@ -47,10 +56,22 @@ const planToRetire = {
 	choices: retirementDateChoices,
 };
 
+const daysOfLeaveChoices = [
+	'Enter number of days [xxx days]',
+	'Calculate assuming the standard max for me',
+];
+
+const daysOfLeave = {
+	type: 'list',
+	name: 'daysOfLeave',
+	message: 'How many days of leave do you plan to have built up?',
+	choices: daysOfLeaveChoices,
+};
+
 const enterRetirementDate = {
 	type: 'input',
 	name: 'retirementDate',
-	message: 'Enter date (01/MM/YYYY)',
+	message: retirementDateChoices[0],
 	validate: validateFirstOfMonthDate,
 };
 
@@ -68,29 +89,53 @@ const enterConusOrOconus = {
 	choices: ['CONUS', 'OCONUS'],
 };
 
+const enterDaysOfLeave = {
+	type: 'input',
+	name: 'daysOfLeaveInput',
+	message: daysOfLeaveChoices[0],
+	validate: validateInputIsNumbersOnly,
+};
+
 const promptUser = async function promptUser() {
-	const userInput = {
-		retirementDate: '',
-		conusOrOcunus: '',
-		daysOfLeave: 0,
-	};
+	const userInput = {};
 
 	const retirementDateChoice = await inquirer.prompt(planToRetire);
 
 	const { planToRetire: dateSource } = retirementDateChoice;
+
+	// Retirment Date
 	if (dateSource === retirementDateChoices[0]) {
-		const retirementDateInput = await inquirer.prompt(enterRetirementDate);
-		userInput.retirementDate = retirementDateInput.retirementDate;
+		const { retirementDate } = await inquirer.prompt(enterRetirementDate);
+		userInput.retirementDate = retirementDate;
 	} else if (dateSource === retirementDateChoices[1]) {
 		// Calculate 20 years
-		const enlistOrCommissionDateInput = await inquirer.prompt(
+		const { enlistOrCommissionDate } = await inquirer.prompt(
 			enterEnlistmentOrCommissionDate
 		);
-		const { enlistOrCommissionDate } = enlistOrCommissionDateInput;
 		userInput.retirementDate = calculateRetirementDate(enlistOrCommissionDate);
 	} else {
-		throw new Error(`Unhandled planToRetire encountered ${planToRetire}`);
+		throw new Error(
+			`Unhandled planToRetire choice encountered ${planToRetire}`
+		);
 	}
+	// End retirement Date
+
+	// CONUS or OCONUS
+	const { conusOrOconus } = await inquirer.prompt(enterConusOrOconus);
+	userInput.CONUSorOCONUS = conusOrOconus;
+	userInput.CONUSorOCONUSDays = conusOrOconus === 'CONUS' ? 20 : 30;
+	// End CONUS or OCONUS
+
+	// Days of leave
+	const daysOfLeaveChoiceInput = await inquirer.prompt(daysOfLeave);
+	if (daysOfLeaveChoiceInput.daysOfLeave === daysOfLeaveChoices[0]) {
+		const { daysOfLeaveInput } = await inquirer.prompt(enterDaysOfLeave);
+		userInput.daysOfLeave = daysOfLeaveInput;
+	} else if (daysOfLeaveChoiceInput.daysOfLeave === daysOfLeaveChoices[1]) {
+	} else {
+		throw new Error(`Unhandled daysOfLeave choice encountered ${planToRetire}`);
+	}
+	// End Days of leave
 
 	console.log(userInput);
 };
@@ -99,8 +144,8 @@ class Retire {
 	constructor() {
 		this.situation = 'I plan to retire';
 		this.plannedRetirementDate = '';
-		this.CONUS = false;
-		this.OCUNUS = false;
+		this.CONUSorOCONUS = '';
+		this.CONUSorOCONUSDays = 0;
 		this.daysOfLeave = 0;
 	}
 
